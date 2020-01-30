7b19b96dbb7eb452f266e8d4778bdcdff6cd7fda
redRain47 <redrain228@gmail.com>
2020-01-30 21:35:03 +0200

    Refactored main classes

---
+++ src/main/java/ua/redrain47/hw11/AppRunner.java
@@ -0,0 +1,8 @@
+package ua.redrain47.hw11;
+
+public class AppRunner {
+    public static void main(String[] args) {
+        AppHelper appHelper = new AppHelper();
+        appHelper.start();
+    }
+}
