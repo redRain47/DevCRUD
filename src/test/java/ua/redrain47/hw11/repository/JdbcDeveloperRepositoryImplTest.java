128a40d77228a898a6b7f70dab86999e5789019f
redRain47 <redrain228@gmail.com>
2020-02-04 19:53:36 +0200

    Added tests of JdbcDeveloperRepositoryImpl

---
+++ src/test/java/ua/redrain47/hw11/repository/JdbcDeveloperRepositoryImplTest.java
@@ -0,0 +1,132 @@
+package ua.redrain47.hw11.repository;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ua.redrain47.hw11.exceptions.DbConnectionIssueException;
+import ua.redrain47.hw11.exceptions.DeletingReferencedRecordException;
+import ua.redrain47.hw11.exceptions.SuchEntityAlreadyExistsException;
+import ua.redrain47.hw11.model.Account;
+import ua.redrain47.hw11.model.AccountStatus;
+import ua.redrain47.hw11.model.Developer;
+import ua.redrain47.hw11.model.Skill;
+import ua.redrain47.hw11.test_util.H2ConnectionUtil;
+import ua.redrain47.hw11.test_util.TempDbManager;
+
+import java.sql.Connection;
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class JdbcDeveloperRepositoryImplTest {
+    private DeveloperRepository developerRepo;
+    private TempDbManager tempDbManager;
+    private Connection connection;
+
+    @Before
+    public void setUp() throws Exception {
+        connection = H2ConnectionUtil.getConnection();
+        developerRepo = new JdbcDeveloperRepositoryImpl(connection);
+        tempDbManager = new TempDbManager(connection);
+        tempDbManager.createDatabase();
+        tempDbManager.populateDatabase();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        connection.close();
+    }
+
+    @Test
+    public void shouldSaveCorrectly() throws DbConnectionIssueException, SuchEntityAlreadyExistsException {
+        Account account = new Account(1L, "asd@sda.com", AccountStatus.ACTIVE);
+        Skill javaSkill = new Skill(1L, "Java");
+        Developer expectedNewDeveloper = new Developer(4L, "John",
+                "Cena", Collections.singleton(javaSkill), account);
+        developerRepo.save(expectedNewDeveloper);
+
+        Developer savedNewDeveloper = developerRepo.getById(4L);
+
+        assertEquals(expectedNewDeveloper, savedNewDeveloper);
+    }
+
+    @Test(expected = SuchEntityAlreadyExistsException.class)
+    public void shouldNotSaveDuplicate() throws SuchEntityAlreadyExistsException, DbConnectionIssueException {
+        Developer duplicatedSkill = developerRepo.getById(1L);
+        developerRepo.save(duplicatedSkill);
+    }
+
+    @Test
+    public void shouldGetDataById() throws DbConnectionIssueException {
+        Account account = new Account(1L, "asd@sda.com", AccountStatus.ACTIVE);
+
+        Set<Skill> skillSet = new HashSet<>();
+        skillSet.add(new Skill(1L, "Java"));
+        skillSet.add(new Skill(2L, "C++"));
+
+        Developer expectedDeveloper = new Developer(1L, "Rob",
+                "Walker", skillSet, account);
+
+        assertEquals(expectedDeveloper, developerRepo.getById(1L));
+    }
+
+    @Test
+    public void shouldNotGetDataByNonExistedId() throws DbConnectionIssueException {
+        assertNull(developerRepo.getById(5L));
+    }
+
+    @Test
+    public void shouldGetAll() throws DbConnectionIssueException {
+        List<Developer> expectedDeveloperList = new ArrayList<>();
+        Skill javaSkill = new Skill(1L, "Java");
+        Skill cppSkill = new Skill(2L, "C++");
+        Skill pythonSkill = new Skill(3L, "Python");
+
+        Set<Skill> firstDevSkillSet = new HashSet<>();
+        firstDevSkillSet.add(javaSkill);
+        firstDevSkillSet.add(cppSkill);
+
+        Set<Skill> secondDevSkillSet = new HashSet<>();
+        secondDevSkillSet.add(pythonSkill);
+        secondDevSkillSet.add(cppSkill);
+
+        expectedDeveloperList.add(new Developer(1L, "Rob",
+                "Walker", firstDevSkillSet, new Account(1L, "asd@sda.com",
+                AccountStatus.ACTIVE)));
+        expectedDeveloperList.add(new Developer(2L, "Mike",
+                "Vazovsky", secondDevSkillSet, new Account(2L,
+                "qqqeqe@gmail.com", AccountStatus.BANNED)));
+        expectedDeveloperList.add(new Developer(3L, "Dexter",
+                "Morgan", Collections.singleton(javaSkill),
+                new Account(3L, "zeref13@yandex.ru", AccountStatus.DELETED)));
+
+        assertEquals(expectedDeveloperList, developerRepo.getAll());
+    }
+
+    @Test
+    public void shouldUpdate() throws DbConnectionIssueException, SuchEntityAlreadyExistsException {
+        Developer updatedSkill = developerRepo.getById(1L);
+
+        updatedSkill.setLastName("Sins");
+        developerRepo.update(updatedSkill);
+
+        assertEquals(updatedSkill, developerRepo.getById(1L));
+    }
+
+    @Test(expected = SuchEntityAlreadyExistsException.class)
+    public void shouldNotUpdateDueToDuplicate() throws DbConnectionIssueException, SuchEntityAlreadyExistsException {
+        Developer developer = developerRepo.getById(1L);
+        developer.setId(2L);
+        developerRepo.update(developer);
+    }
+
+    @Test
+    public void shouldDeleteCorrectly() throws DbConnectionIssueException, DeletingReferencedRecordException {
+        List<Developer> allDevelopersButOne = developerRepo.getAll();
+
+        allDevelopersButOne.remove(0);
+        developerRepo.deleteById(1L);
+
+        assertEquals(allDevelopersButOne, developerRepo.getAll());
+    }
+}
