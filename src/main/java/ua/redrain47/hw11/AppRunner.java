3ce470f2ce4958703becb8e9f9e7f0dcd614f6c7
redRain47 <redrain228@gmail.com>
2020-02-08 13:12:17 +0200

    Deleted redundant classes

--- src/main/java/ua/redrain47/hw11/AppRunner.java
+++
@@ -1,8 +0,0 @@
-package ua.redrain47.hw11;
-
-public class AppRunner {
-    public static void main(String[] args) {
-        AppHelper appHelper = new AppHelper();
-        appHelper.start();
-    }
-}
