a4177a094ceaecfe3a0f913c4eed503b96f7eafc
redRain47 <redrain228@gmail.com>
2020-03-03 00:18:47 +0200

    Added configuration for spring mvc

--- src/main/java/ua/redrain47/devcrud/config/RepositoryConfig.java
+++ src/main/java/ua/redrain47/devcrud/config/RepositoryConfig.java
@@ -3,7 +3,7 @@ package ua.redrain47.devcrud.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import ua.redrain47.devcrud.exceptions.DbConnectionIssueException;
+import ua.redrain47.devcrud.exceptions.DbStorageException;
 import ua.redrain47.devcrud.repository.AccountRepository;
 import ua.redrain47.devcrud.repository.DeveloperRepository;
 import ua.redrain47.devcrud.repository.SkillRepository;
@@ -15,15 +15,15 @@ import ua.redrain47.devcrud.repository.jdbc.JdbcSkillRepositoryImpl;
 @ComponentScan
 public class RepositoryConfig {
     @Bean(name = "skillRepository")
-    public SkillRepository skillRepository() throws DbConnectionIssueException {
+    public SkillRepository skillRepository() throws DbStorageException {
         return new JdbcSkillRepositoryImpl();
     }
     @Bean(name = "accountRepository")
-    public AccountRepository accountRepository() throws DbConnectionIssueException {
+    public AccountRepository accountRepository() throws DbStorageException {
         return new JdbcAccountRepositoryImpl();
     }
     @Bean(name = "developerRepository")
-    public DeveloperRepository developerRepository() throws DbConnectionIssueException {
+    public DeveloperRepository developerRepository() throws DbStorageException {
         return new JdbcDeveloperRepositoryImpl();
     }
 }
